93179336e19d985da375d5f70c898667982986e4
Spyder337 <owsley.wood@gmail.com>
2024-12-27 11:08:12 -0800

    Update migrations to include tables for tasks.
    Updated: 2024-12-27 11:08:12
    
    Files Changed:
    M migrations/2024-12-23-072302_init/down.sql
    M migrations/2024-12-23-072302_init/up.sql

--- migrations/2024-12-23-072302_init/down.sql
+++ migrations/2024-12-23-072302_init/down.sql
@@ -1,3 +1,5 @@
 -- This file should undo anything in `up.sql`
 DROP TABLE IF EXISTS quotes;
 DROP TABLE IF EXISTS searches;
+DROP TABLE IF EXISTS task_relations;
+DROP TABLE IF EXISTS tasks;
--- migrations/2024-12-23-072302_init/up.sql
+++ migrations/2024-12-23-072302_init/up.sql
@@ -1,13 +1,43 @@
 -- Your SQL goes here
 CREATE TABLE IF NOT EXISTS searches (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    -- Search query parameter
     query TEXT NOT NULL,
+    -- Site to limit the search to
     website TEXT,
+    -- Required search term
     allintext TEXT,
+    -- Time the search was created
     time_stamp DATE NOT NULL
 );
 CREATE TABLE IF NOT EXISTS quotes (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    -- Quote text
     quote TEXT NOT NULL,
+    -- Author of the quote
     author TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS tasks(
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    -- Task name
+    task TEXT NOT NULL,
+    -- Optional description of the task
+    desc TEXT,
+    -- in progress, completed, incompleted
+    status TEXT NOT NULL,
+    -- time the task was created
+    time_stamp DATE NOT NULL,
+    -- time the task is due
+    due_date DATE,
+    -- renewal duration
+    renewal_duration INT
+);
+CREATE TABLE IF NOT EXISTS task_relations(
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    -- Parent task
+    parent_id INT NOT NULL,
+    -- Child task
+    child_id INT NOT NULL,
+    FOREIGN KEY (parent_id) REFERENCES tasks(id),
+    FOREIGN KEY (child_id) REFERENCES tasks(id)
 );
