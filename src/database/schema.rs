db38e20516871e085c6806795da64b7749a6915a
Spyder337 <owsley.wood@gmail.com>
2024-12-27 11:15:32 -0800

    Updated diesel migrations.
    Updated: 2024-12-27 11:15:32
    
    Files Changed:
    M src/database/schema.rs

--- src/database/schema.rs
+++ src/database/schema.rs
@@ -18,7 +18,29 @@ diesel::table! {
     }
 }
 
+diesel::table! {
+    task_relations (id) {
+        id -> Integer,
+        parent_id -> Integer,
+        child_id -> Integer,
+    }
+}
+
+diesel::table! {
+    tasks (id) {
+        id -> Integer,
+        task -> Text,
+        desc -> Nullable<Text>,
+        status -> Text,
+        time_stamp -> Date,
+        due_date -> Nullable<Date>,
+        renewal_duration -> Nullable<Integer>,
+    }
+}
+
 diesel::allow_tables_to_appear_in_same_query!(
     quotes,
     searches,
+    task_relations,
+    tasks,
 );
