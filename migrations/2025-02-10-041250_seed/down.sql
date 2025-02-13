51ec3b3b59a7cff555078bc95734662eef0f5678
Spyder337 <owsley.wood@gmail.com>
2025-02-12 21:43:08 -0800

    Added initializing some default quotes.
    Updated: 2025-02-13 05:43:08
    
    Files Changed:
    M migrations/2025-02-10-041250_seed/down.sql
    M migrations/2025-02-10-041250_seed/up.sql

--- migrations/2025-02-10-041250_seed/down.sql
+++ migrations/2025-02-10-041250_seed/down.sql
@@ -1,3 +1,3 @@
 -- This file should undo anything in `up.sql`
 
-DELETE FROM quotes *;
+DELETE FROM quotes;
--- migrations/2025-02-10-041250_seed/up.sql
+++ migrations/2025-02-10-041250_seed/up.sql
@@ -1,25 +1,25 @@
 -- Your SQL goes here
 
-INSERT INTO quotes (quote, author) VALUES ("The real problem in speech is not precise language. The problem is clear language. ... \nIt is really quite impossible to say anything with absolute precision, unless that thing is so abstracted from the real world as to not represent any real thing.", "Richard Feynman");
+INSERT INTO quotes (id, quote, author) VALUES (0, "The real problem in speech is not precise language. The problem is clear language. ... \nIt is really quite impossible to say anything with absolute precision, unless that thing is so abstracted from the real world as to not represent any real thing.", "Richard Feynman");
 
-INSERT INTO quotes (quote, author) VALUES ("A very great deal more truth can become known than can be proven.", "Richard Feynman");
+INSERT INTO quotes (id, quote, author) VALUES (1, "A very great deal more truth can become known than can be proven.", "Richard Feynman");
 
-INSERT INTO quotes (quote, author) VALUES ("We've learned from experience that the truth will out.", "Richard Feynman");
+INSERT INTO quotes (id, quote, author) VALUES (2, "We've learned from experience that the truth will out.", "Richard Feynman");
 
-INSERT INTO quotes (quote, author) VALUES ("The first principle is that you must not fool yourself — and you are the easiest person to fool.", "Richard Feynman");
+INSERT INTO quotes (id, quote, author) VALUES (3, "The first principle is that you must not fool yourself — and you are the easiest person to fool.", "Richard Feynman");
 
-INSERT INTO quotes (quote, author) VALUES ("Only the descent into the hell of self-knowledge can pave the way to godliness.", "Immanuel Kant");
+INSERT INTO quotes (id, quote, author) VALUES (4, "Only the descent into the hell of self-knowledge can pave the way to godliness.", "Immanuel Kant");
 
-INSERT INTO quotes (quote, author) VALUES ("The child must be brought up free (that he allow others to be free). ... \nHe must not be obliged to dissimulate, he must acquire immediate horror of lies, must learn so to respect the rights of men that they become an insurmountable wall for him.", "Immanuel Kant");
+INSERT INTO quotes (id, quote, author) VALUES (5, "The child must be brought up free (that he allow others to be free). ... \nHe must not be obliged to dissimulate, he must acquire immediate horror of lies, must learn so to respect the rights of men that they become an insurmountable wall for him.", "Immanuel Kant");
 
-INSERT INTO quotes (quote, author) VALUES ("Character means that the person derives his rules of conduct from himself and from the dignity of humanity.", "Immanuel Kant");
+INSERT INTO quotes (id, quote, author) VALUES (6, "Character means that the person derives his rules of conduct from himself and from the dignity of humanity.", "Immanuel Kant");
 
-INSERT INTO quotes (quote, author) VALUES ("Mathematics would certainly have not come into existence if one had known from the beginning that there was in nature no exactly straight line, no actual circle, no absolute magnitude.", "Friedrich Nietzsche");
+INSERT INTO quotes (id, quote, author) VALUES (7, "Mathematics would certainly have not come into existence if one had known from the beginning that there was in nature no exactly straight line, no actual circle, no absolute magnitude.", "Friedrich Nietzsche");
 
-INSERT INTO quotes (quote, author) VALUES ("He who fights with monsters should see to it that he himself does not become a monster. And if you gaze long into an abyss, the abyss also gazes into you.", "Friedrich Nietzsche");
+INSERT INTO quotes (id, quote, author) VALUES (8, "He who fights with monsters should see to it that he himself does not become a monster. And if you gaze long into an abyss, the abyss also gazes into you.", "Friedrich Nietzsche");
 
-INSERT INTO quotes (quote, author) VALUES ("The liar is a person who uses the valid designations, the words, in order to make something which is unreal appear to be real.", "Friedrich Nietzsche");
+INSERT INTO quotes (id, quote, author) VALUES (9, "The liar is a person who uses the valid designations, the words, in order to make something which is unreal appear to be real.", "Friedrich Nietzsche");
 
-INSERT INTO quotes (quote, author) VALUES ("The man who is guided by concepts and abstractions only succeeds by such means in warding off misfortune, without ever gaining any happiness for himself from these abstractions.\nAnd while he aims for the greatest possible freedom from pain, the intuitive man, standing in the midst of a culture, already reaps from his intuition a harvest of continually inflowing illumination, cheer, and redemption — in addition to obtaining a defense against misfortune.", "Friederich Nietzsche");
+INSERT INTO quotes (id, quote, author) VALUES (10, "The man who is guided by concepts and abstractions only succeeds by such means in warding off misfortune, without ever gaining any happiness for himself from these abstractions.\nAnd while he aims for the greatest possible freedom from pain, the intuitive man, standing in the midst of a culture, already reaps from his intuition a harvest of continually inflowing illumination, cheer, and redemption — in addition to obtaining a defense against misfortune.", "Friederich Nietzsche");
 
-INSERT INTO quotes (quote, author) VALUES ("Life is, after all, not a product of morality.", "Friederich Nietzsche");
+INSERT INTO quotes (id, quote, author) VALUES (11, "Life is, after all, not a product of morality.", "Friederich Nietzsche");
